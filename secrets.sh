7bf94c05c1c9b5fb9b86f968677320ecef90628b
Muhammad Asim <quickbooks2018@gmail.com>
2022-07-09 10:39:01 +0500

    source

--- secrets.sh
+++ secrets.sh
@@ -1,3 +1,7 @@
 export USERNAME=$(aws secretsmanager get-secret-value --secret-id secrets --region us-east-1 --query SecretString --output text | jq -r '."USERNAME"')
 
 export PASSWORD=$(aws secretsmanager get-secret-value --secret-id secrets --region us-east-1 --query SecretString --output text | jq -r '."PASSWORD"')
+
+echo $USERNAME > taha.txt 
+
+echo $PASSWORD >> taha.txt
