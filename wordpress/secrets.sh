f48dee863ad21fa612292651a23d8f7fb074eae3
Muhammad Asim <quickbooks2018@gmail.com>
2022-07-10 07:22:58 +0500

    env

--- wordpress/secrets.sh
+++ wordpress/secrets.sh
@@ -1,9 +1,11 @@
-export WORDPRESS_DB_HOST=$(aws secretsmanager get-secret-value --secret-id wordpress --region us-east-1 --query SecretString --output text | jq -r '."WORDPRESS_DB_HOST"')
-export WORDPRESS_DB_USER=$(aws secretsmanager get-secret-value --secret-id wordpress --region us-east-1 --query SecretString --output text | jq -r '."WORDPRESS_DB_USER"')
-export WORDPRESS_DB_PASSWORD=$(aws secretsmanager get-secret-value --secret-id wordpress --region us-east-1 --query SecretString --output text | jq -r '."WORDPRESS_DB_PASSWORD"')
-export WORDPRESS_DB_NAME=mysql
+export WORDPRESS_DATABASE_HOST=$(aws secretsmanager get-secret-value --secret-id wordpress --region us-east-1 --query SecretString --output text | jq -r '."WORDPRESS_DATABASE_HOST"')
+export WORDPRESS_DATABASE_USER=$(aws secretsmanager get-secret-value --secret-id wordpress --region us-east-1 --query SecretString --output text | jq -r '."WORDPRESS_DATABASE_USER"')
+export WORDPRESS_DATABASE_PASSWORD=$(aws secretsmanager get-secret-value --secret-id wordpress --region us-east-1 --query SecretString --output text | jq -r '."WORDPRESS_DATABASE_PASSWORD"')
+export WORDPRESS_DATABASE_PORT_NUMBER=$(aws secretsmanager get-secret-value --secret-id wordpress --region us-east-1 --query SecretString --output text | jq -r '."WORDPRESS_DATABASE_PORT_NUMBER"')
+export WORDPRESS_DATABASE_NAME=mysql
 
-echo $WORDPRESS_DB_HOST > wordpress.txt
-echo $WORDPRESS_DB_USER >> wordpress.txt
-echo $WORDPRESS_DB_PASSWORD >> wordpress.txt
-echo $WORDPRESS_DB_NAME >> wordpress.txt
+echo $WORDPRESS_DATABASE_HOST > wordpress.txt
+echo $WORDPRESS_DATABASE_USER >> wordpress.txt
+echo $WORDPRESS_DATABASE_PASSWORD >> wordpress.txt
+echo $WORDPRESS_DATABASE_NAME >> wordpress.txt
+echo $WORDPRESS_DATABASE_PORT_NUMBER >> wordpress.txt
