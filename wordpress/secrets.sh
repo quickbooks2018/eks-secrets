5f76af31196ece4fec7e3d473826243ab332e7dc
Muhammad Asim <quickbooks2018@gmail.com>
2022-07-11 08:27:03 +0500

    update

--- wordpress/secrets.sh
+++ wordpress/secrets.sh
@@ -1,11 +1,9 @@
-export WORDPRESS_DATABASE_HOST=$(aws secretsmanager get-secret-value --secret-id wordpress --region us-east-1 --query SecretString --output text | jq -r '."WORDPRESS_DATABASE_HOST"')
-export WORDPRESS_DATABASE_USER=$(aws secretsmanager get-secret-value --secret-id wordpress --region us-east-1 --query SecretString --output text | jq -r '."WORDPRESS_DATABASE_USER"')
-export WORDPRESS_DATABASE_PASSWORD=$(aws secretsmanager get-secret-value --secret-id wordpress --region us-east-1 --query SecretString --output text | jq -r '."WORDPRESS_DATABASE_PASSWORD"')
-export WORDPRESS_DATABASE_PORT_NUMBER=$(aws secretsmanager get-secret-value --secret-id wordpress --region us-east-1 --query SecretString --output text | jq -r '."WORDPRESS_DATABASE_PORT_NUMBER"')
-export WORDPRESS_DATABASE_NAME=mysql
+export WORDPRESS_DB_HOST=$(aws secretsmanager get-secret-value --secret-id wordpress --region us-east-1 --query SecretString --output text | jq -r '."WORDPRESS_DB_HOST"')
+export WORDPRESS_DB_USER=$(aws secretsmanager get-secret-value --secret-id wordpress --region us-east-1 --query SecretString --output text | jq -r '."WORDPRESS_DB_USER"')
+export WORDPRESS_DB_PASSWORD=$(aws secretsmanager get-secret-value --secret-id wordpress --region us-east-1 --query SecretString --output text | jq -r '."WORDPRESS_DB_PASSWORD"')
+export WORDPRESS_DB_NAME=mysql
 
-echo WORDPRESS_DATABASE_HOST=$WORDPRESS_DATABASE_HOST > /secrets
-echo WORDPRESS_DATABASE_USER=$WORDPRESS_DATABASE_USER >> /secrets
-echo WORDPRESS_DATABASE_PASSWORD=$WORDPRESS_DATABASE_PASSWORD >> /secrets
-echo WORDPRESS_DATABASE_NAME=$WORDPRESS_DATABASE_NAME >> /secrets
-echo WORDPRESS_DATABASE_PORT_NUMBER=$WORDPRESS_DATABASE_PORT_NUMBER >> /secrets
+echo WORDPRESS_DB_HOST=$WORDPRESS_DB_HOST > /secrets
+echo WORDPRESS_DB_USER=$WORDPRESS_DB_USER >> /secrets
+echo WORDPRESS_DB_PASSWORD=$WORDPRESS_DB_PASSWORD >> /secrets
+echo WORDPRESS_DB_NAME=$WORDPRESS_DB_NAME >> /secrets
