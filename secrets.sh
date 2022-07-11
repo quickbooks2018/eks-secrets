695b23b47163dfa677f829d68f9e5614c768776d
Muhammad Asim <quickbooks2018@gmail.com>
2022-07-11 12:02:25 +0500

    secrets

--- secrets.sh
+++ secrets.sh
@@ -2,6 +2,6 @@ export USERNAME=$(aws secretsmanager get-secret-value --secret-id secrets --regi
 
 export PASSWORD=$(aws secretsmanager get-secret-value --secret-id secrets --region us-east-1 --query SecretString --output text | jq -r '."PASSWORD"')
 
-echo USERNAME=$USERNAME > secrets
+echo USERNAME=$USERNAME > /secrets
 
-echo PASSWORD=$PASSWORD >> secrets
+echo PASSWORD=$PASSWORD >> /secrets
