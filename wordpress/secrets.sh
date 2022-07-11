7be14be563eb7cc3df38cdaa3ef7f701e1a7d77d
Muhammad Asim <quickbooks2018@gmail.com>
2022-07-11 10:09:37 +0500

    secrets

--- wordpress/secrets.sh
+++
@@ -1,9 +0,0 @@
-export WORDPRESS_DB_HOST=$(aws secretsmanager get-secret-value --secret-id wordpress --region us-east-1 --query SecretString --output text | jq -r '."WORDPRESS_DB_HOST"')
-export WORDPRESS_DB_USER=$(aws secretsmanager get-secret-value --secret-id wordpress --region us-east-1 --query SecretString --output text | jq -r '."WORDPRESS_DB_USER"')
-export WORDPRESS_DB_PASSWORD=$(aws secretsmanager get-secret-value --secret-id wordpress --region us-east-1 --query SecretString --output text | jq -r '."WORDPRESS_DB_PASSWORD"')
-export WORDPRESS_DB_NAME=mysql
-
-echo WORDPRESS_DB_HOST=$WORDPRESS_DB_HOST > /secrets
-echo WORDPRESS_DB_USER=$WORDPRESS_DB_USER >> /secrets
-echo WORDPRESS_DB_PASSWORD=$WORDPRESS_DB_PASSWORD >> /secrets
-echo WORDPRESS_DB_NAME=$WORDPRESS_DB_NAME >> /secrets
